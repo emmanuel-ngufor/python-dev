cff3b611ef863633aa07967e2bc1aaf152bebaa8
Emmanuel Ngufor <esngufor@myseneca.ca>
2024-12-03 23:00:18 -0500

    updated directory progress

---
+++ Day 1/task/task.py
@@ -0,0 +1,6 @@
+print('Welcome to the BandName Generator App')
+
+city = input("What city did you grow up in?: \n")
+pet = input("What is the name of your pet?: \n")
+
+print("Your Band name is " + city + pet)
--- Day 25/us_states_game/main.py
+++ Day 25/us_states_game/main.py
@@ -19,12 +19,14 @@ while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's another state's name?").title()
 
     if answer_state == "Exit":
-        missed_states = []
-        for state in all_states:
-            if state not in guessed_states:
-                missed_states.append(state)
+        # missed_states = []
+        # for state in all_states:
+        #     if state not in guessed_states:
+        #         missed_states.append(state)
+        missed_states = [state for state in all_states if state not in guessed_states]
         new_data = pd.DataFrame(missed_states)
         new_data.to_csv("states_to_learn.csv")
+
         break
 
     if answer_state in all_states:
