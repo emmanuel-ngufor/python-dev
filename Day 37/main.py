b8576215ec632ab02e4cecf6b0967359372181f6
Emmanuel Ngufor <esngufor@myseneca.ca>
2024-12-25 00:07:45 -0500

    added .env file for security configurations

--- Day 37/main.py
+++ Day 37/main.py
@@ -1,24 +1,29 @@
+import os 
 import requests
 from datetime import datetime
+from dotenv import load_dotenv
 
-USERNAME = "emman"
-TOKEN = "bbgu&387sb!~*)idu:>?\]+\"`<>,.dhjh"
+load_dotenv()  # take environment variables from .env.
+
+USERNAME =  os.environ.get("user_name")
+TOKEN = os.environ.get("token")
 GRAPH_ID = "graph1"
 
-pixela_endpoint = "https://pixe.la/v1/users"
 
+# CREATE USER PROFILE
+pixela_endpoint = "https://pixe.la/v1/users"
 user_params = {
     "token": TOKEN,
     "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes"
 }
-# User profile acc created.
 # response = requests.post(url=pixela_endpoint, json=user_params)
 # print(response.text)
 
-graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 
+# GRAPH CREATION
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 graph_config = {
     "id": GRAPH_ID,
     "name": "Coding Graph",
@@ -30,10 +35,11 @@ graph_config = {
 headers = {
     "X-USER-TOKEN": TOKEN
 }
-
 # response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
 # print(response.text)
 
+
+# PIXEL CREATION
 pixel_creation_endpoint = f"{graph_endpoint}/{GRAPH_ID}"
 
 today = datetime.now()
@@ -48,22 +54,18 @@ response = requests.post(url=pixel_creation_endpoint, json=pixel_data_config, he
 print(response.text)
 
 
-
+# UPDATING PIXEL
 yesterday = datetime(year=2024, month=12, day=23)
 yesterday = yesterday.strftime("%Y%m%d")
 pixel_update_endpoint = f"{graph_endpoint}/{GRAPH_ID}/{yesterday}"
 pixel_data_config = {
     "quantity": "0.35",
 }
-
 # response = requests.put(url=pixel_update_endpoint, json=pixel_data_config, headers=headers)
 # print(response.text)
 
-delete_endpoint = f"{graph_endpoint}/{GRAPH_ID}/{yesterday}"
 
+# DELETING A PIXEL
+delete_endpoint = f"{graph_endpoint}/{GRAPH_ID}/{yesterday}"
 # response = requests.delete(url=delete_endpoint, json=pixel_data_config, headers=headers)
 # print(response.text)
-
-
-
-
