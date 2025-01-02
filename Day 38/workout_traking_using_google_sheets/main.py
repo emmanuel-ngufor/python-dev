a4af2495a6ec5e035dceb50a35dd1a94bc29ee5b
Emmanuel Ngufor <esngufor@myseneca.ca>
2025-01-01 22:19:42 -0500

    started workout tracking app using google sheets

---
+++ Day 38/workout_traking_using_google_sheets/main.py
@@ -0,0 +1,25 @@
+import os 
+from dotenv import load_dotenv
+import requests
+
+load_dotenv()  # take environment variables from .env.
+
+APP_ID =  os.environ.get("nutritionix_app_id")
+API_KEY = os.environ.get("nutritionix_api_key")
+
+natural_lang_for_exercise_url = "https://trackapi.nutritionix.com/v2/natural/exercise"
+
+
+parameters = {
+    "query": "I ran 1 mile"
+}
+
+headers = {
+    "x-app-id": APP_ID,
+    "x-api-key": API_KEY
+}
+
+response = requests.post(url=natural_lang_for_exercise_url, json=parameters, headers=headers)
+response.raise_for_status()
+
+print(response.text)
